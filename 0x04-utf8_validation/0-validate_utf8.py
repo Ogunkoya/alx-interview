ddfdb004c0315194b31f73a95777faa5591eb733
Ogunkoya <godrace062@yahoo.com>
2023-05-18 23:53:34 +0100

    Task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
 def validUTF8(data):
-  """
-  Returns True if the given data set represents a valid UTF-8 encoding, else returns False.
+    """
+    Returns True if the given data set represents a valid UTF-8 encoding, else returns False.
 
-  Args:
-    data: A list of integers representing the data set.
+    Args:
+      data: A list of integers representing the data set.
 
-  Returns:
-    True if data is a valid UTF-8 encoding, else False.
-  """
-  num_bytes = 0
+    Returns:
+      True if data is a valid UTF-8 encoding, else False.
+    """
+    number_bytes = 0
 
-  for byte in data:
-    # Check if the byte is a continuation byte
-    if num_bytes == 0:
-      if byte >> 5 == 0b110:
-        num_bytes = 1
-      elif byte >> 4 == 0b1110:
-        num_bytes = 2
-      elif byte >> 3 == 0b11110:
-        num_bytes = 3
-      elif byte >> 7 == 0b0:
-        continue
-      else:
-        return False
-    else:
-      # Check if the byte is a valid continuation byte
-      if byte >> 6 != 0b10:
-        return False
-      num_bytes -= 1
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-  # Check if there are any remaining bytes
-  return num_bytes == 0
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
