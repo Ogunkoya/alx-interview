64036f310d30998911a5326079190283f4d8b34d
Ogunkoya <godrace062@yahoo.com>
2023-06-02 09:58:47 +0100

    Task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,44 @@
 #!/usr/bin/node
 
-const axios = require('request');
+const request = require('request');
 
-// Function to fetch movie details
-async function fetchMovieCharacters(movieId) {
-  try {
-    // Fetch movie details using the Star Wars API
-    const response = await request.get(`https://swapi.dev/api/films/${movieId}/`);
+function printCharacters(movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
 
-    // Extract the characters array from the response
-    const characters = response.data.characters;
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const filmData = JSON.parse(body);
+      const characters = filmData.characters;
+      let characterCount = 0;
 
-    // Fetch and print character names
-    for (const characterUrl of characters) {
-      const characterResponse = await request.get(characterUrl);
-      console.log(characterResponse.data.name);
+      function fetchCharacterData(characterUrl) {
+        request(characterUrl, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const characterData = JSON.parse(body);
+            const characterName = characterData.name;
+            console.log(characterName);
+
+            characterCount++;
+            if (characterCount === characters.length) {
+              // All characters printed, script execution complete
+              process.exit();
+            }
+          } else {
+            console.log(`Failed to fetch character data: ${response.statusCode}`);
+          }
+        });
+      }
+
+      characters.forEach(fetchCharacterData);
+    } else {
+      console.log(`Failed to fetch film data: ${response.statusCode}`);
     }
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+  });
 }
 
-// Call the function with the movie ID as the command-line argument
-const movieId = process.argv[2];
-fetchMovieCharacters(movieId);
+if (process.argv.length < 3) {
+  console.log("Please provide a movie ID as a command line argument.");
+} else {
+  const movieId = process.argv[2];
+  printCharacters(movieId);
+}
