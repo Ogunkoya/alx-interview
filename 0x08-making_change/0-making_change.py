5e049ab78af1bf252b78b581e8b5799e7e799548
Ogunkoya <godrace062@yahoo.com>
2023-06-15 23:54:46 +0100

    Task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
 def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given total amount.
+
+    Args:
+        coins (list): A list of the values of the coins in your possession.
+        total (int): The total amount to be met using the coins.
+
+    Returns:
+        int: The fewest number of coins needed to meet the total.
+             If the total is 0 or less, returns 0.
+             If the total cannot be met by any number of coins, returns -1.
+
+    """
+
     if total <= 0:
         return 0
 
