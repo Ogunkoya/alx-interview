ad8c6a606c638dc48b2827bf83ba2d8e41d3c721
Ogunkoya <godrace062@yahoo.com>
2023-06-15 23:57:40 +0100

    Task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+'''Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet
+    a given amount total.
+'''
+import sys
+
 
 def makeChange(coins, total):
     """
@@ -14,27 +20,17 @@ def makeChange(coins, total):
              If the total cannot be met by any number of coins, returns -1.
 
     """
-
     if total <= 0:
         return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
 
-    # Create a list to store the fewest number of coins needed for each amount from 0 to total
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
-
-    # Iterate through all possible amounts from 1 to total
-    for amount in range(1, total + 1):
-        # Iterate through each coin value
-        for coin in coins:
-            # Check if the coin value is less than or equal to the current amount
-            if coin <= amount:
-                # Calculate the number of coins needed for the current amount
-                num_coins = min_coins[amount - coin] + 1
-                # Update the fewest number of coins if it is smaller than the current value
-                min_coins[amount] = min(min_coins[amount], num_coins)
-
-    # Return the fewest number of coins needed to meet the total
-    if min_coins[total] == float('inf'):
+    if table[total] == sys.maxsize:
         return -1
-    else:
-        return min_coins[total]
